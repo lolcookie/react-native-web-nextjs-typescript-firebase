25a6b49766f552cb5eadaae06077066bc85f741e
Dylan Broadbridge <dylan.broadbridge@gmail.com>
2019-06-07 18:19:58 +0800

    example of failing to build

--- app/next.config.js
+++ app/next.config.js
@@ -1,14 +1,18 @@
 const withTypescript = require('@zeit/next-typescript')
+const withTM = require('next-transpile-modules')
 
-module.exports = withTypescript({
-  webpack: config => {
-    // Alias all `react-native` imports to `react-native-web`
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      'react-native$': 'react-native-web'
-    }
+module.exports = withTypescript(
+  withTM({
+    transpileModules: ['react-native-popup-menu'],
+    webpack: config => {
+      // Alias all `react-native` imports to `react-native-web`
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        'react-native$': 'react-native-web'
+      }
 
-    return config
-  },
-  distDir: '../dist/functions/next'
-})
+      return config
+    },
+    distDir: '../dist/functions/next'
+  })
+)
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import {
+  MenuProvider,
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger
+} from 'react-native-popup-menu'
 
 const styles = StyleSheet.create({
   container: {
@@ -13,8 +20,28 @@ const styles = StyleSheet.create({
   }
 })
 
-export default () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Welcome to Next.js!</Text>
-  </View>
-)
+const Index = (): JSX.Element => {
+  return (
+    <MenuProvider>
+      <View>
+        <Text>Hello world!</Text>
+        <Menu>
+          <MenuTrigger text="Select action" />
+          <MenuOptions>
+            <MenuOption onSelect={() => alert(`Save`)} text="Save" />
+            <MenuOption onSelect={() => alert(`Delete`)}>
+              <Text style={{ color: 'red' }}>Delete</Text>
+            </MenuOption>
+            <MenuOption
+              onSelect={() => alert(`Not called`)}
+              disabled={true}
+              text="Disabled"
+            />
+          </MenuOptions>
+        </Menu>
+      </View>
+    </MenuProvider>
+  )
+}
+
+export default Index
